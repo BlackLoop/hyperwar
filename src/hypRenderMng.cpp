21084c8d1f0405df69e02aaeac4478c6260b6a21
colin <colin.bouvry@gmail.com>
2014-12-04 21:34:02 +0100

    Fred: mouse tracking

--- src/hypRenderMng.cpp
+++ src/hypRenderMng.cpp
@@ -73,12 +73,67 @@ void hypRenderMng::RenderPlay()
 
 	//ofEnableLighting();
 
+    static float s_fLastMouseX = 0.f;
+    static float s_fLastMouseY = 0.f;
+
+	float fCenterX = 1024.f / 2.0f;
+	float fCenterY = 768.f / 2.0f;
+
+    float fMouseFromCenterX =  m_mouseX - fCenterX;
+    float fMouseFromCenterY =  m_mouseY - fCenterY;
+
+    float fMouseXNorm = (2.f*fMouseFromCenterX) / 1024.f;
+    float fMouseYNorm = (2.f*fMouseFromCenterY) / 768.f;
+
+    float I_BKGND_WIDTH  = 8*1920;
+    float I_BKGND_HEIGHT = 8*1360;
+
+    float fOverflowX = 8*1920;// - 1024;
+    float fOverflowY = 8*1360;// - 768;
+
+    static float s_fCurrentX = -fOverflowX/2.f;
+	static float s_fCurrentY = -fOverflowY/2.f;
+
+    static float s_fTargetX = 0.f;
+    static float s_fTargetY = 0.f;
+
+    if (m_mouseX != s_fLastMouseX || m_mouseY != s_fLastMouseY) {
+        s_fTargetX = -fOverflowX/2.f + fMouseXNorm * I_BKGND_WIDTH/2.0f;
+        s_fTargetY = -fOverflowY/2.f + fMouseYNorm * I_BKGND_HEIGHT/2.0f;
+    }
+
     ofPushMatrix();
-    ofTranslate( m_mouseX ,m_mouseY, m_zoom);
+
+    ofVec2f vDist(s_fTargetX - s_fCurrentX,
+                  s_fTargetX - s_fCurrentY);
+
+    if (vDist.length() > 1.f) {
+
+        if (s_fCurrentX < s_fTargetX) {
+            s_fCurrentX += 1.0f;
+        }
+        else {
+            s_fCurrentX += -1.0f;
+        }
+
+        if (s_fCurrentY < s_fTargetY) {
+            s_fCurrentY += 1.0f;
+        }
+        else {
+            s_fCurrentY += -1.0f;
+        }
+    }
+
+    ofTranslate(s_fCurrentX,
+                s_fCurrentY,
+                -100.0f);
+
     RenderBackground();
     RenderAnimations();
     ofPopMatrix();
 
+    s_fLastMouseX = m_mouseX;
+    s_fLastMouseY = m_mouseY;
 }
 
 void hypRenderMng::RenderBackground()

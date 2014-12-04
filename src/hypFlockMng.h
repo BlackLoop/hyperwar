be685564a5266179b896279c81d2952bee4f0840
barzilouik <barzilouik@gmail.com>
2014-12-04 17:56:49 +0100

    [ADD] Flocking shits

---
+++ src/hypFlockMng.h
@@ -0,0 +1,29 @@
+#pragma once
+
+#include "ofMain.h"
+#include "ofApp.h"
+
+#include "Singleton.h"
+
+#include "ofxFlocking.h"
+
+#include <map>
+#include <string>
+
+class hypFlockMng : public Singleton<hypFlockMng> {
+public:
+
+	hypFlockMng();
+	virtual ~hypFlockMng();
+
+	void Setup();
+	void Render();
+
+	void Update();
+
+    ofxFlocking flock;
+
+private:
+};
+
+typedef Singleton<hypFlockMng> hypFlockMngSingleton;
